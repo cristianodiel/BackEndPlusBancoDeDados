c25f11387055cc37224c92da202f3187a3b22b25
cristiano diel <cristianodiel@gmail.com>
2022-11-29 18:03:54 -0300

    aula 22-11-29 part2

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const express = require("express")
 const fs = require('fs')
 const app = express()
 
+app.use(express.json())
+
 let dados =[]
 
 function getDados(){
     dados = JSON.parse(fs.readFileSync('./db.json'))
 
 }
+function escreveDados (){
+    fs.writeFileSync('./db.json', JSON.stringify(dados))
+
+}
+
 
 app.get('/users',function(req,res){
     const dados = fs.readFileSync('./db.json')
@@ -25,6 +32,20 @@ app.get('/users/:id',function(req,res){
     res.send(usuario)
     
 })
+app.post("/users", function(req,res){
+    const novoUsuario ={
+        nome:req.body.nome,
+        idade: req.body.idade
+    }
+    console.log(novoUsuario)
+    getDados()
+    //novoUsuario.id=???
+    dados.push(novoUsuario)
+    escreveDados()
+    res.sendStatus(201)
+
+})
+
 app.listen(3000, function () {
     console.log("Servidor conectado")
 })
